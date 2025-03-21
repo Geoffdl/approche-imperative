2f39f48b78317f04ec8859fa24c7a8e7bc18009e
Geoffdl <geoffroydelorme@gmail.com>
2025-03-21 18:39:55 +0100

    pick 1-10

--- src/fr/algorithmie/InteractifTantQue.java
+++ src/fr/algorithmie/InteractifTantQue.java
@@ -6,21 +6,23 @@ public class InteractifTantQue {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int nb = scanner.nextInt();
 
+        int nb;
         boolean isValid = false;
+
 
         while(!isValid){
             System.out.println("Please enter a number between 1 and 10: ");
-
-            if(nb>= 1 && nb <= 10)  {
-                System.out.println("Good job, this is a great number" + nb);
+            nb = scanner.nextInt();
+            if(nb>=1 && nb <=10){
+                System.out.println("Good job! "+ nb + " is also my favorite number");
                 isValid = true;
-            } else {
+            }
+            else {
                 System.out.println("Wrong, try again");
+
             }
         }
-
 
     }
 }
